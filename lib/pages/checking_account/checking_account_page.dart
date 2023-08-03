47c946e948bef6b049c555376941113ddf655675
Savio <savioluislobo@gmail.com>
2023-08-03 18:19:08 -0300

    Creating the rest of the Pages

---
+++ lib/pages/checking_account/checking_account_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CheckingAccountPage extends StatelessWidget {
+  const CheckingAccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Checking Account Page"),
+      ),
+    );
+  }
+}
