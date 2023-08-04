bac12963633d3ee5b31eb42a2e83419e24633d0f
savio <96928404+savioluis@users.noreply.github.com>
2023-08-04 18:58:15 -0300

    Adding routes in the app (#3)

--- lib/widgets/features.dart
+++ lib/widgets/features.dart
@@ -1,4 +1,9 @@
 import 'package:camp/models/user_model/feature.dart';
+import 'package:camp/pages/checking_account/checking_account_page.dart';
+import 'package:camp/pages/credit_cards/credit_cards_page.dart';
+import 'package:camp/pages/payment/payment_page.dart';
+import 'package:camp/pages/pix/pix_page.dart';
+import 'package:camp/pages/transfer/transfer_page.dart';
 import 'package:camp/shared/app_colors.dart';
 import 'package:camp/shared/app_settings.dart';
 import 'package:flutter/material.dart';
@@ -8,35 +13,77 @@ class FeaturesWidget extends StatelessWidget {
   final List<Feature> features;
   const FeaturesWidget({super.key, required this.features});
 
-  Widget getFeature(String icon, String description) {
-    return Card(
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(12))
+
+  void goToPage(BuildContext context, String description) {
+    if (description == "Pagar") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const PaymentPage(),
         ),
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        width: 140,
-        height: 125,
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.network(
-              icon,
-              // semanticsLabel: description,
-              colorFilter: ColorFilter.mode(AppColors.red, BlendMode.srcIn),
-              width: 36,
-              height: 36,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              description,
-              style: TextStyle(fontSize: 16, color: AppColors.grayLight),
-            )
-          ],
+      );
+    } else if (description == "Transferir") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const TransferPage(),
+        ),
+      );
+    } else if (description == "Conta Corrente") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const CheckingAccountPage(),
+        ),
+      );
+    } else if (description == "CartÃµes") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const CreditCardsPage(),
+        ),
+      );
+    } else if (description == "PIX") {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const PixPage(),
+        ),
+      );
+    }
+  }
+
+  Widget getFeature(BuildContext context, String icon, String description) {
+    return InkWell(
+      onTap: () => goToPage(context, description),
+      child: Card(
+        child: Container(
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(12))),
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          width: 140,
+          height: 125,
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.network(
+                icon,
+                // semanticsLabel: description,
+                colorFilter: ColorFilter.mode(AppColors.red, BlendMode.srcIn),
+                width: 36,
+                height: 36,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                description,
+                style: TextStyle(fontSize: 16, color: AppColors.grayLight),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -51,7 +98,7 @@ class FeaturesWidget extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: features
-            .map((e) => getFeature(e.icon!, e.description!))
+            .map((e) => getFeature(context, e.icon!, e.description!))
             .toList(),
       ),
     );
